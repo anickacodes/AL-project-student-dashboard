b741c49167841d20d26a602d5f96a92d435f61c3
anickacodes <anickalewis@pursuit.org>
2023-08-03 12:50:45 -0400

    updated REACT render, successfully accessed student names and photos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import studentData from "./data/data.json"
+
+
+console.log("start", studentData)
+
+const studentsToRender = studentData.map(
+  (eachStudentObj)=>{return(
+    <div>
+  <img src="{eachStudentObj.profilePhoto} "alt={`${eachStudentObj.names.preferredName}`} />
+  <h4>{eachStudentObj.username}</h4>
+  </div>)}
+  )
+//   console.log(studentsToRender)
+//  console.log(studentData[0].names)
 
 function App() {
   return (
+    <>
     <div>
       <h1>Student Dashboard</h1>
     </div>
+    {studentsToRender}
+    </>
   );
 }
 
